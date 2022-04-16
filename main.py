04dc2ab9e516a57bcdc21f8d4d3957f933955337
Tanupats <Tanupat_s@kkumail.com>
2022-04-16 23:39:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -944,7 +944,8 @@ def getcoordinate():
     c = cos
     a = 0.5 -c((lat2 - lat1) * p) / 2 +c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2
     answer=12742 * asin(sqrt(a))
-    return {"duration":answer}
+
+    return jsonify({"duration":answer} ) 
    
 
 
